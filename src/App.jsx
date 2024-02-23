2892c340a78754115208f7e29989e3b1e3b53229
Pushpendra <pushpendrasingh.91650@gmail.com>
2024-02-24 03:20:20 +0530

    first componant

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Chai from "./Chai";
+
+
+
 function App() {
 
   return (
     <>
       <h1>Chai or react | Pushpendra</h1>
+
+      <Chai />
     </>
   )
 }
---
+++ src/Chai.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chai = () => {
+  return (
+    <div>
+      <h2>Chai component</h2>
+    </div>
+  )
+}
+
+export default Chai
