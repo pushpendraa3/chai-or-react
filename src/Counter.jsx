f2242d3f0f2f33ca53ab97b9f5687a92d753a3d8
Pushpendra <pushpendrasingh.91650@gmail.com>
2024-02-24 17:09:44 +0530

    removed unused code from counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,11 +3,6 @@ import React, { useState } from 'react'
 const Counter = () => {
     const [count, setCount] = useState(0);
 
-    const addCount = () => {
-        // console.log("value added")
-        // const newCount = count + 1;
-        setCount( count + 1 )
-    }
   return (
     <div>
         <h2>Count: {count}</h2>
